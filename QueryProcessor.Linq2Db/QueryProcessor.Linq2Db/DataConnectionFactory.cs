56503c544452d133c2d5295c85437c02ee926544
divanchikov <d.ivanchikov@mrock.center>
2020-07-05 19:37:34 +0300

    fiesrt version

---
+++ QueryProcessor.Linq2Db/QueryProcessor.Linq2Db/DataConnectionFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LinqToDB.Data;
+using LinqToDB.DataProvider.PostgreSQL;
+
+namespace QueryProcessor.Linq2Db {
+    public interface IDataConnectionFactory {
+        DataConnection CreateConnection();
+    }
+
+    public class DataConnectionFactory : IDataConnectionFactory {
+        private readonly Func<DataConnection> _func;
+
+        public DataConnectionFactory(Func<DataConnection> func) {
+            _func = func;
+        }
+
+        public DataConnection CreateConnection() {
+            return _func();
+        }
+    }
+    
+    
+  /*  public class DecisionSystemDataConnection : DataConnection {
+        public DecisionSystemDataConnection(string connectionString)
+            : base(new PostgreSQLDataProvider(), connectionString) {
+            TurnTraceSwitchOn();
+            WriteTraceLine = (message, displayName) => {
+                Console.WriteLine($"{message} {displayName}");
+            };
+        }
+
+    }*/
+}
---
+++ QueryProcessor.Linq2Db/Sandbox/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LinqToDB.Data;
+using LinqToDB.DataProvider.PostgreSQL;
+using QueryProcessor.Linq2Db;
+
+namespace Sandbox {
+    class Program {
+        private const string connectionString = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=123";
+
+        static async Task Main(string[] args) {
+            var connectionFactory = new DataConnectionFactory((() => new DecisionSystemDataConnection(connectionString) {
+            }));
+            try {
+                var type = typeof(Test);
+
+                var requestProcessor = new RequestProcessor(connectionFactory);
+                var result = await requestProcessor.Process<Test>(new RequestModel {
+                    Filters = new List<Filter> {
+                        new Filter {
+                            Field = "Name",
+                            Value = "Dima",
+                            FilterType = FilterType.Contains
+                        },
+                        new Filter {
+                            Field = "Id",
+                            Value = "Dima",
+                            FilterType = FilterType.Contains
+                        },
+                        new Filter {
+                            Field = "Age",
+                            Value = "3",
+                            FilterType = FilterType.From
+                        },
+                        new Filter {
+                            Field = "Age",
+                            Value = "7",
+                            FilterType = FilterType.To
+                        }
+                    },
+                    Sort = new Sort {
+                        Direction = Direction.Desc,
+                        Field = "Id"
+                    },
+                    Paging = new Paging {
+                        PageNumber = 1,
+                        PageSize = 1
+                    }
+                });
+            } catch (Exception exception) {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+        
+    }
+    
+}
